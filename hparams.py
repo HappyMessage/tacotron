8b1ebcfb8300640eb293e8c23cf4b657a363c00c
begeekmyfriend <begeekmyfriend@gmail.com>
2018-05-09 10:50:54 +0800

    Synchronize with upstream
    Signed-off-by: begeekmyfriend <begeekmyfriend@gmail.com>

--- hparams.py
+++ hparams.py
@@ -5,21 +5,21 @@ import tensorflow as tf
 hparams = tf.contrib.training.HParams(
   # Comma-separated list of cleaners to run on text prior to training and eval. For non-English
   # text, you may want to use "basic_cleaners" or "transliteration_cleaners" See TRAINING_DATA.md.
-  cleaners='english_cleaners',
+  cleaners='basic_cleaners',
 
   # Audio:
   num_mels=80,
   num_freq=1025,
-  min_mel_freq=125,
+  min_mel_freq=50,
   max_mel_freq=7600,
-  sample_rate=20000,
+  sample_rate=16000,
   frame_length_ms=50,
   frame_shift_ms=12.5,
   min_level_db=-100,
   ref_level_db=20,
 
   # Model:
-  outputs_per_step=5,
+  outputs_per_step=2,
 
   embedding_dim=512,
 
@@ -51,7 +51,7 @@ hparams = tf.contrib.training.HParams(
   use_cmudict=False,  # Use CMUDict during training to learn pronunciation of ARPAbet phonemes
 
   # Eval:
-  max_iters=200,
+  max_iters=1000,
   griffin_lim_iters=60,
   power=1.5,              # Power to raise magnitudes to prior to Griffin-Lim
 )
